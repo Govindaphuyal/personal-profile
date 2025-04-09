325bba08c9d0e704a7d062ca1528e2b941d7ecd8
Govindaphuyal <govindaphuyal40@gmail.com>
2025-04-09 17:37:09 +0545

    update main

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -5,6 +5,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { CiFacebook } from "react-icons/ci";
 import { CiLinkedin } from "react-icons/ci";
 import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 const roles = [
   "React/Next Developer",
@@ -59,17 +60,22 @@ const Main = () => {
           <div className="mt-5 sm:flex md:mt-8">
             <div className="rounded-md shadow">
               <a
-               href="/Frontend Developer.pdf" // Path to the PDF file in the public folder
-  download="Govinda_Phuyal_CV.pdf"
+               href="/Frontend Developer.pdf" 
+              download="Govinda_Phuyal_CV.pdf"
                 className="flex items-center justify-center w-full px-6 py-3 text-base font-medium text-white transition duration-300 ease-in-out bg-blue-500 border border-transparent rounded-md hover:bg-gray-600 focus:outline-none focus:shadow-outline-blue md:py-4 md:text-lg md:px-8 hover:outline-2 outline-offset-2 outline-cyan-500"
               >
                 Download CV
               </a>
             </div>
             <div className="flex gap-4 mt-4 sm:mt-0 sm:ml-4">
-              <CiFacebook className="text-blue-400 text-xl sm:text-2xl" />
+              
+              
+              <Link href="https://www.linkedin.com/in/govinda-phuyal-222344203/">
               <CiLinkedin className="text-blue-400 text-xl sm:text-2xl" />
+              </Link>
+              <Link href="https://github.com/Govindaphuyal">
               <FaGithub className="text-blue-400 text-xl sm:text-2xl" />
+              </Link>
             </div>
           </div>
         </div>
@@ -78,7 +84,7 @@ const Main = () => {
           <div className="relative w-full p-3 rounded md:p-8">
             <div className="w-48 h-50 sm:w-64 sm:h-64 md:w-72 md:h-70 lg:w-96 lg:h-96 border-8 border-blue-600 rounded-full">
               <img
-                src="https://picsum.photos/400/300"
+                src="/49587666_106971730389699_1088202184316682240_n (1).jpg"
                 alt="Profile"
                 className="w-full h-full rounded-full object-cover"
               />
