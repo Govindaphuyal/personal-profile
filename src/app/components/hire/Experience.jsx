e1d32b36d4f02156ff846b02976831c88c471644
Govindaphuyal <govindaphuyal40@gmail.com>
2025-04-09 17:44:22 +0545

    up company

--- src/app/components/hire/Experience.jsx
+++ src/app/components/hire/Experience.jsx
@@ -5,7 +5,7 @@ const Experience = () => {
     
          <div>
             <h1 className='text-4xl text-white'>My <span className='text-2xl text-green-400 font-bold'>Experiance</span></h1>
-            <p className='w-180 text-2xl mt-4 mb-4 text-white'>With <span className="text-green-400">intern in easysoft</span> years of experience as a Web Developer,  Frontend & Backend Developer, I create high-performing websites, apps, and marketing strategies. From SEO-driven web design to scalable applications, I deliver user-centric, responsive, and conversion-focused solutions that drive growth. 🚀</p>
+            <p className='w-180 text-2xl mt-4 mb-4 text-white'>With <span className="text-green-400">3-month intern in easysoft</span> years of experience as a Web Developer,  Frontend & Backend Developer, I create high-performing websites, apps, and marketing strategies. From SEO-driven web design to scalable applications, I deliver user-centric, responsive, and conversion-focused solutions that drive growth. 🚀</p>
           <div className="bg-gray-600  text-white px-4 py-1 rounded">
         <div class="bg-grey-200 rounded-lg shadow-lg overflow-hidden max-w-lg w-full">
         <div class="p-6">
